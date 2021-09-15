85f8a52281c84159091deba7474a1174d72e5b25
JorgeGarcia855 <jlgarciap@unbosque.edu.co>
2021-09-14 22:54:41 -0500

    Table sort

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 var rowId = 0
 
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+const dbName = "petDB";
 
+var request = indexedDB.open(dbName, 2);
 
+request.onerror = function(event) {
+  console.log("Database error");
+};
+request.onupgradeneeded = function(event) {
+  var db = event.target.result;
+  var objectStore = db.createObjectStore("pets", { keyPath: "id" });
+  objectStore.createIndex("petNameInput", "petNameInput", { unique: false });
+};
+
+var request = indexedDB.open(dbName, 2);
+request.onsuccess = function(event) {
+	var db = event.target.result;
+	var tx = db.transaction("pets");
+	var objectStore = tx.objectStore("pets");
+	objectStore.getAll().onsuccess = function(event) {
+	  //console.log(event.target.result);
+	  rowId = event.target.result.length;
+	};
+};
 
 document.getElementById("petsave-button").onclick = function () {
-    
-   
-    
     rowId++
     let pet = {
         dateInput: document.getElementById("date-input").value,
@@ -21,7 +40,13 @@ document.getElementById("petsave-button").onclick = function () {
         localityInput: document.getElementById("locality-input").value
     }
 
-    
+    var request = indexedDB.open(dbName, 2);
+ 	  request.onsuccess = function(event) {
+    	var db = event.target.result;
+    	var customerObjectStore = db.transaction("pets", "readwrite").objectStore("pets");
+	    pet["id"] = rowId;
+	    customerObjectStore.add(pet);
+  	};
 
     let tr = document.createElement("tr")
     tr.setAttribute("id", "row-" + rowId)
@@ -52,6 +77,17 @@ document.getElementById("petsave-button").onclick = function () {
     tdActions.appendChild(input)
     tr.appendChild(tdActions)
     document.getElementById("body-table").appendChild(tr)
-
+    
+    
+    
+    
 }
 
+function addDogImage() {
+  var dogImg = document.createElement("img")
+  dogImg.src = fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json)
+                  .then(data => {
+                  
+                  })
+  document.getElementById("pet-image").appendChild(dogImg)
+}
